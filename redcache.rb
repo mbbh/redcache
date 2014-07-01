02fb6107684042437ec5b6094403df6a4ab3ed36
Martin Hauser <mh@wrongexit.de>
2014-07-01 16:26:27 +0200

    move class calls to instance calls to favour several delimiters

--- redcache.rb
+++ redcache.rb
@@ -3,39 +3,38 @@ require 'redis'
 REDCACHE_INTERNAL_DELIM = "!DELIM-REDCACHE!"
 
 class RedCache
-  private_class_method :new
 
-  def self.setup(delim="/")
-    @redis = Redis.new(:host => "127.0.0.1")
-    @curpath = delim
-    @delim = delim
-  end
-
-  def self.cache_path(path)
+  def cache_path(path)
     paths = path[0].to_s == @delim ?
       path.split(@delim) :
       @curpath.split(@delim) + path.split(@delim)
       return paths[0].empty? ? paths[1..-1] : paths
   end
 
-  def self.cache_path_serialized(path)
+  def cache_path_serialized(path)
     cache_path(path).join(REDCACHE_INTERNAL_DELIM)
   end
 
-  def self.unserialize_paths(paths)
+  def unserialize_paths(paths)
     @curpath + paths.split(REDCACHE_INTERNAL_DELIM).join(@delim)
   end
 
-  def self.set_path(path, file)
+  def initialize(delim="/")
+    @redis = Redis.new(:host => "127.0.0.1")
+    @curpath = delim
+    @delim = delim
+  end
+
+  def set_path(path, file)
     @redis.set cache_path_serialized(path), Marshal.dump(file)
   end
 
-  def self.get_path(path, default=nil)
+  def get_path(path, default=nil)
     data = @redis.get(cache_path_serialized(path))
     return data ? Marshal.load(data) : default
   end
 
-  def self.get_nodes_at(path)
+  def get_nodes_at(path)
     paths = cache_path(path)
     paths << "*"
     cursor = -1
--- redcache_test.rb
+++ redcache_test.rb
@@ -8,14 +8,14 @@ def run(name, bool)
   end
 end
 
-RedCache.setup
-RedCache.set_path("/foo/bar", "1234")
-RedCache.set_path("foo/bar", "5555")
-run "simple get/set", RedCache.get_path("foo/bar") == "5555"
+rc = RedCache.new
+rc.set_path("/foo/bar", "1234")
+rc.set_path("foo/bar", "5555")
+run "simple get/set", rc.get_path("foo/bar") == "5555"
 
 keys = (1..55).to_a.map {|i| "/foo/x/baz#{i}"}
 keys.each do |k|
-  RedCache.set_path(k, "1234")
+  rc.set_path(k, "1234")
 end
 
-run "get_nodes_below", RedCache.get_nodes_at("foo/x/").sort == keys.sort
+run "get_nodes_below", rc.get_nodes_at("foo/x/").sort == keys.sort
